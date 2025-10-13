3c08a758b798bb679391421d3b6b040a5903a55b
FaysArukattil <faysarukattil@gmail.com>
2025-10-13 17:43:06 +0530

    EOD

--- lib/views/add_post_screen/add_post_screen.dart
+++ lib/views/add_post_screen/add_post_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram/data/dummy_data.dart';
 import 'package:instagram/models/post_model.dart';
-import 'package:instagram/models/reel_model.dart';
 import 'package:instagram/views/bottomnavbarscreens/bottomnavbarscreen.dart';
+import 'package:instagram/views/reels_editor_screen/reels_editor_screen.dart';
 import 'package:instagram/views/story_editing_screen/story_editor_screen.dart';
 
 class AddPostScreen extends StatefulWidget {
@@ -623,102 +623,6 @@ class _PostEditorScreenState extends State<PostEditorScreen> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-// ---------------- Reel Editor ----------------
-class ReelEditorScreen extends StatefulWidget {
-  final String videoPath;
-  const ReelEditorScreen({super.key, required this.videoPath});
-
-  @override
-  State<ReelEditorScreen> createState() => _ReelEditorScreenState();
-}
-
-class _ReelEditorScreenState extends State<ReelEditorScreen> {
-  final TextEditingController _captionController = TextEditingController();
-
-  @override
-  void dispose() {
-    _captionController.dispose();
-    super.dispose();
-  }
-
-  void _shareReel() {
-    final newReel = ReelModel(
-      id: 'reel_${DateTime.now().millisecondsSinceEpoch}',
-      userId: DummyData.currentUser.id,
-      videoUrl: widget.videoPath,
-      thumbnailUrl: widget.videoPath,
-      caption: _captionController.text.trim(),
-      likes: 0,
-      comments: 0,
-      shares: 0,
-      timeAgo: 'Just now',
-    );
-
-    DummyData.reels.insert(0, newReel);
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Reel posted successfully!'),
-        backgroundColor: Colors.green,
-        duration: Duration(seconds: 2),
-      ),
-    );
-
-    Navigator.pop(context);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.pop(context),
-        ),
-        title: const Text('New reel', style: TextStyle(color: Colors.black)),
-        actions: [
-          TextButton(
-            onPressed: _shareReel,
-            child: const Text(
-              'Share',
-              style: TextStyle(color: Colors.blue, fontSize: 16),
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.videocam, size: 100, color: Colors.black),
-                  const SizedBox(height: 16),
-                  Text('Video: ${widget.videoPath.split('/').last}'),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.grey[200],
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: _captionController,
-              decoration: const InputDecoration(
-                hintText: 'Add a caption...',
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
--- lib/views/reels_editor_screen/reels_editor_screen.dart
+++ lib/views/reels_editor_screen/reels_editor_screen.dart
@@ -1,1 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/data/dummy_data.dart';
+import 'package:instagram/models/reel_model.dart';
 
+class ReelEditorScreen extends StatefulWidget {
+  final String videoPath;
+  const ReelEditorScreen({super.key, required this.videoPath});
+
+  @override
+  State<ReelEditorScreen> createState() => _ReelEditorScreenState();
+}
+
+class _ReelEditorScreenState extends State<ReelEditorScreen> {
+  final TextEditingController _captionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _captionController.dispose();
+    super.dispose();
+  }
+
+  void _shareReel() {
+    final newReel = ReelModel(
+      id: 'reel_${DateTime.now().millisecondsSinceEpoch}',
+      userId: DummyData.currentUser.id,
+      videoUrl: widget.videoPath,
+      thumbnailUrl: widget.videoPath,
+      caption: _captionController.text.trim(),
+      likes: 0,
+      comments: 0,
+      shares: 0,
+      timeAgo: 'Just now',
+    );
+
+    DummyData.reels.insert(0, newReel);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Reel posted successfully!'),
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 2),
+      ),
+    );
+
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text('New reel', style: TextStyle(color: Colors.black)),
+        actions: [
+          TextButton(
+            onPressed: _shareReel,
+            child: const Text(
+              'Share',
+              style: TextStyle(color: Colors.blue, fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.videocam, size: 100, color: Colors.black),
+                  const SizedBox(height: 16),
+                  Text('Video: ${widget.videoPath.split('/').last}'),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              controller: _captionController,
+              decoration: const InputDecoration(
+                hintText: 'Add a caption...',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
